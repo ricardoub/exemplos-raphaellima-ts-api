7087b2f64098367a556ebec951340b61f7ca6449
Ricardo Bomfim <ricardoub@gmail.com>
2017-10-02 13:42:46 -0300

    aula32 - Criando o módulo config

---
+++ build/config/env/development.env.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: 'development',
+    db: 'ts-api',
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'postgres',
+    host: 'localhost',
+    serverPort: 3000,
+    dbPort: 5432,
+    dbURL: 'postgres://postgres:postgres@localhost:5432/ts-api',
+    secret: 's3cr3t'
+};
