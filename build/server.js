63466d9ea78e1415444d837009884ebeae7e1cc5
Ricardo Bomfim <ricardoub@gmail.com>
2017-10-02 13:52:03 -0300

    aula33 - Importando os módulos

--- build/server.js
+++ build/server.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var http = require("http");
-var server = http.createServer();
-server.listen(3000, function () { return console.log('Server está rodando na porta 3000'); });
+var api_1 = require("./api/api");
+var errorHandlerApi_1 = require("./api/errorHandlerApi");
+var config = require('./config/env/config')();
+var server = http.createServer(api_1.default);
+api_1.default.use(errorHandlerApi_1.errorHandlerApi);
+server.listen(config.serverPort, function () { return console.log('Server está rodando na porta 3000'); });
