0bc7d9f3dd7ba129dae2f164ebcfa41d96230581
Ricardo Bomfim <ricardoub@gmail.com>
2017-10-03 14:07:19 -0300

    Aula41- unit.test

---
+++ build/tests/unit/unit.test.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+describe('Testes de integração do controller', function () {
+    describe('Método Create', function () {
+        it('Deve criar um novo usuário', function () {
+        });
+    });
+    describe('Método Update', function () {
+        it('Deve atualizar um Usuário', function () {
+        });
+    });
+    describe('Método GET users', function () {
+        it('Deve retornar uma lista com todos os usuários', function () {
+        });
+    });
+    describe('Método Delete', function () {
+        it('Deve deletar um Usuário', function () {
+        });
+    });
+});
