cb64be5bfd21b51f5689b707b3d1f201e9e56eee
Ricardo Bomfim <ricardoub@gmail.com>
2017-10-05 14:36:29 -0300

    aula44 - Craindo o modulo user

--- server/api/routes/routes.ts
+++ server/api/routes/routes.ts
@@ -7,8 +7,13 @@ class Routes {
   }
 
   getRoutes(app: Application): void {
-    app.route('/').get((req: Request, res: Response) => res.send('Hello, world!'));
-    app.route('/ola/:nome').get((req: Request, res: Response) => res.send(`Hello, ${req.params.nome}`));
+    // app.route('/').get((req: Request, res: Response) => res.send('Hello, world!'));
+    // app.route('/ola/:nome').get((req: Request, res: Response) => res.send(`Hello, ${req.params.nome}!`));
+    app.route('/api/users/all').get(this.router.index);
+    app.route('/api/users/create').post(this.router.create);
+    app.route('/api/users/:id').get(this.router.findOne);
+    app.route('/api/users/:id/update').put(this.router.update);
+    app.route('/api/users/:id/destroy').put(this.router.destroy);
   }
 
 }
