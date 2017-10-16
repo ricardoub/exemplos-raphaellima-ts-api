014a213df3f8222fee4bc74a0c38814d8a7e85b8
Ricardo Bomfim <ricardoub@gmail.com>
2017-10-16 10:08:13 -0200

    aula68 - Refatorando testes unitários - testando o método service delete

--- build/tests/unit/unit.test.js
+++ build/tests/unit/unit.test.js
@@ -5,24 +5,33 @@ var service_1 = require("../../server/modules/User/service");
 describe('Testes unitários do controller', function () {
     describe('USUÁRIO: Método GET users', function () {
         it('Deve retornar uma lista com todos os usuários', function () {
-        });
-    });
-    describe('USUÁRIO: Método Create', function () {
-        it('Deve criar um novo usuário', function () {
-            var novoUsuario = {
-                id: 1,
-                name: 'Novo usuario',
-                email: 'novousuario@gmail.com',
-                password: '1234'
-            };
             var user = new service_1.default();
             return user
-                .create(novoUsuario)
+                .getAll()
                 .then(function (data) {
-                helpers_1.expect(data.dataValues).to.have.all.keys(['email', 'id', 'name', 'password', 'updatedAt', 'createdAt']);
+                helpers_1.expect(data).to.be.an('array');
+                helpers_1.expect(data[0]).to.have.all.keys(['email', 'id', 'name', 'password']);
             });
         });
     });
+    // describe('USUÁRIO: Método Create', () => {
+    //   it('Deve criar um novo usuário', () => {
+    //     const novoUsuario = {
+    //       id: 1,
+    //       name: 'Novo usuario',
+    //       email: 'novousuario@gmail.com',
+    //       password: '1234'
+    //     };
+    //     const user = new User();
+    //     return user
+    //       .create(novoUsuario)
+    //       .then(data => {
+    //         expect(data.dataValues).to.have.all.keys(
+    //           ['email', 'id', 'name', 'password', 'updatedAt', 'createdAt']
+    //         )
+    //       });
+    //   });
+    // });
     // describe('USUÁRIO: Método Update', () => {
     //   it('Deve atualizar um Usuário', () => {
     //
--- tests/unit/unit.test.ts
+++ tests/unit/unit.test.ts
@@ -5,28 +5,36 @@ describe('Testes unitários do controller', () => {
 
   describe('USUÁRIO: Método GET users', () => {
     it('Deve retornar uma lista com todos os usuários', () => {
-
-    });
-  });
-
-  describe('USUÁRIO: Método Create', () => {
-    it('Deve criar um novo usuário', () => {
-      const novoUsuario = {
-        id: 1,
-        name: 'Novo usuario',
-        email: 'novousuario@gmail.com',
-        password: '1234'
-      };
       const user = new User();
       return user
-        .create(novoUsuario)
+        .getAll()
         .then(data => {
-          expect(data.dataValues).to.have.all.keys(
-            ['email', 'id', 'name', 'password', 'updatedAt', 'createdAt']
+          expect(data).to.be.an('array');
+          expect(data[0]).to.have.all.keys(
+            ['email', 'id', 'name', 'password']
           )
-        });
+        })
     });
   });
+
+  // describe('USUÁRIO: Método Create', () => {
+  //   it('Deve criar um novo usuário', () => {
+  //     const novoUsuario = {
+  //       id: 1,
+  //       name: 'Novo usuario',
+  //       email: 'novousuario@gmail.com',
+  //       password: '1234'
+  //     };
+  //     const user = new User();
+  //     return user
+  //       .create(novoUsuario)
+  //       .then(data => {
+  //         expect(data.dataValues).to.have.all.keys(
+  //           ['email', 'id', 'name', 'password', 'updatedAt', 'createdAt']
+  //         )
+  //       });
+  //   });
+  // });
 
   // describe('USUÁRIO: Método Update', () => {
   //   it('Deve atualizar um Usuário', () => {
